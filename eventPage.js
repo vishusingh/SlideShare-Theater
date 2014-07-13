ad769e356dc4be53e170b291ec050163deeb547d
Jai Pandya <jaipandya@gmail.com>
2014-07-14 01:03:11 +0530

    Use keyboard shortcut Alt+Shift+S to start SlideShare Theater

--- eventPage.js
+++ eventPage.js
@@ -1,5 +1,13 @@
 chrome.browserAction.onClicked.addListener( function (tab) {
+  startTheater();
+})
+chrome.commands.onCommand.addListener(function(command) {
+  if (command = 'start-ss-theater'){
+    startTheater();
+  }
+});
+function startTheater(){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {startTheater: true});
   });
-})
+}
