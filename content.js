48516fffe60ececf527b91932ee79f85f471add7
Vishu <vishu@Macintosh-5.local>
2014-07-13 20:36:21 +0530

    Presentation navigator implemented

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener( function(request, sender){
     if( theater.length > 0 ) {
       closeTheater();
     } else {
-      $('body').addClass('theater-running').append('<div id="ss-theater"/>');
+      $('body').addClass('theater-running').append('<div id="ss-theater"/><div id="theater-slides-list"><a id="thumbs-list-handle"></a><ul class="slide-thumbs"/></div>');
       var theater = $('#ss-theater');
 
       $('#ss-theater').load(chrome.extension.getURL('ss-theater.html'), function () {
@@ -26,34 +26,64 @@ chrome.runtime.onMessage.addListener( function(request, sender){
 
         var theaterSlides = $('.slide_image');
         var slideUrls = [];
+        var thumbUrls = [];
+
         for (var i = theaterSlides.length - 1; i >= 0; i--) {
           slideUrls.push(theaterSlides[i].getAttribute('data-full'));
+          thumbUrls.push(theaterSlides[i].getAttribute('data-normal'));
         };
 
         var slidesList = $('#theater-slides');
+        var thumbsListContainer = $('#theater-slides-list');
+        var thumbsList = $('.slide-thumbs', '#theater-slides-list');
         totalSlides = slideUrls.length;
 
         for (var i = 0; i <= slideUrls.length - 1; i++) {
-          slidesList.append('<li><img src="' + slideUrls[totalSlides - (i+1)] + '"/><span class="slide-number">' + (i+1) + '/' + slideUrls.length +'</span></li>');
+          slidesList.append('<li id="' + (i+1) + '"><img src="' + slideUrls[totalSlides - (i+1)] + '"/><span class="slide-number">' + (i+1) + '/' + slideUrls.length +'</span></li>');
+          thumbsList.append('<li><a href="#' + (i+1) + '"><img src="' + thumbUrls[totalSlides - (i+1)] + '"/></a></li>');
         };
 
         setTimeout(function () {
           theaterContent.show().addClass('animated fadeInUp');
+          thumbsListContainer.show().addClass('animated fadeInUp');
         }, 600);
+
+        $('#thumbs-list-handle').click(function() {
+          thumbsListContainer.toggleClass('closed');
+        })
+
+        $('a', '#theater-slides-list').click(function(e) {
+          e.preventDefault();
+          var theater = $('#ss-theater');
+
+          var scrollId = $(this).attr('href');
+          var height = $(scrollId).height();
+          var slideNumber = parseInt(scrollId.match(/#([0-9]+)/)[1]);
+
+          theater.animate({
+            scrollTop: (slideNumber - 1) * (height + 35)
+          }, 500);
+        });
+
       });
-    }
+    } //Else block ends
 
     $(document).keyup(function (e) {
       if (e.keyCode == 27) { closeTheater(); }
     })
 
+
     function closeTheater() {
+      var theater = $('#ss-theater');
+      var thumbsListContainer = $('#theater-slides-list');
+
       theater.addClass('animated fadeOutDownBig');
+      thumbsListContainer.addClass('animated fadeOutDownBig');
       setTimeout(function() {
         theater.remove();
+        thumbsListContainer.remove();
         $('body').removeClass('theater-running');
       }, 600);
     }
-
   }
 });
