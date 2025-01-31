50faa35bc40576722d01ac71e537d3182bbd804b
Vishu <visingh.in@gmail.com>
2025-02-01 00:20:25 +0100

    Updated to Manifest v3, jquery update
    - jQuery updated to 3.7.1 from 2.1.1
    - Now support for manifest v3

--- content.js
+++ content.js
@@ -1,105 +1,144 @@
-chrome.runtime.onMessage.addListener( function(request, sender){
-  if( request.startTheater ) {
-    var theater = $('#ss-theater');
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  if (request.startTheater) {
+    var theater = $("#ss-theater");
 
-    if( theater.length > 0 ) {
+    if (theater.length > 0) {
       closeTheater();
     } else {
-      $('body').addClass('theater-running').append('<div id="ss-theater"/><div id="theater-slides-list"><a id="thumbs-list-handle"></a><ul class="slide-thumbs"/></div>');
-      var theater = $('#ss-theater');
+      $("body")
+        .addClass("theater-running")
+        .append(
+          '<div id="ss-theater"></div><div id="theater-slides-list"><a id="thumbs-list-handle"></a><ul class="slide-thumbs"></ul></div>'
+        );
+      var theater = $("#ss-theater");
 
-      $('#ss-theater').load(chrome.extension.getURL('ss-theater.html'), function () {
-        var theaterHeaderContainer = $('.theater-header-container');
-        var theaterContent = $('#theater-content');
-        var slideshowTitle = $('.slideshow-title-text').text();
-        var authorName = $('.j-author-name').text();
-        var theaterTitle = $('.slideshare-title', '#theater-content');
-        var theaterAuthor = $('.theater-author', '#theater-content');
+      $("#ss-theater").load(
+        chrome.runtime.getURL("ss-theater.html"),
+        function () {
+          var theaterHeaderContainer = $(".theater-header-container");
+          var theaterContent = $("#theater-content");
+          var slideshowTitle = $(".title").text();
+          var authorName = $('[data-cy="author-link"]').text();
+          var theaterTitle = $(".slideshare-title", "#theater-content");
+          var theaterAuthor = $(".theater-author", "#theater-content");
 
-        theaterTitle.text(slideshowTitle);
-        theaterAuthor.text('by ' + authorName);
+          theaterTitle.text(slideshowTitle);
+          theaterAuthor.text("by " + authorName);
 
-        var ssLogo = chrome.extension.getURL('/images/ss-logo.png');
-        theaterHeaderContainer.append('<img src="' + ssLogo + '"/>');
+          var ssLogo = chrome.runtime.getURL("/images/ss-logo.png");
+          theaterHeaderContainer.append('<img src="' + ssLogo + '"/>');
 
-        theater.show().addClass('animated fadeInUp');
+          theater.show().addClass("animated fadeInUp");
 
-        var theaterSlides = $('.slide_image');
-        var slideUrls = [];
-        var thumbUrls = [];
+          var theaterSlides = $(".vertical-slide-image");
+          var slideUrls = [];
+          var thumbUrls = [];
 
-        for (var i = theaterSlides.length - 1; i >= 0; i--) {
-          slideUrls.push(theaterSlides[i].getAttribute('data-full'));
-          thumbUrls.push(theaterSlides[i].getAttribute('data-normal'));
-        };
+          for (var i = theaterSlides.length - 1; i >= 0; i--) {
+            const srcset = theaterSlides[i].getAttribute("srcset");
 
-        var slidesList = $('#theater-slides');
-        var thumbsListContainer = $('#theater-slides-list');
-        var thumbsList = $('.slide-thumbs', '#theater-slides-list');
-        totalSlides = slideUrls.length;
+            if (srcset) {
+              const urls = srcset.split(",").map((item) => item.trim()); // Split and clean
+              const firstUrl = urls[0].split(" ")[0]; // Get the first URL (smallest image)
+              const lastUrl = urls[urls.length - 1].split(" ")[0]; // Get last URL (largest image)
 
-        for (var i = 0; i <= slideUrls.length - 1; i++) {
-          slidesList.append('<li id="' + (i+1) + '"><img src="' + slideUrls[totalSlides - (i+1)] + '"/><span class="slide-number">' + (i+1) + '/' + slideUrls.length +'</span></li>');
-          thumbsList.append('<li><a class="slide-thumb" href="#' + (i+1) + '"><img src="' + thumbUrls[totalSlides - (i+1)] + '"/></a></li>');
-        };
-
-        setTimeout(function () {
-          theaterContent.show().addClass('animated fadeInUp');
-          thumbsListContainer.show().addClass('animated fadeInUp');
-        }, 600);
-
-        setTimeout(function () {
-          $('#esc-help').show().addClass('animated fadeInRight');
-        }, 1000);
-
-        setTimeout(function () {
-          thumbsListContainer.addClass('closed');
-        }, 3000);
-
-        theater.scroll(function(){
-          var escHelp = $('#esc-help');
-          if(theater.scrollTop() > 0) {
-            escHelp.addClass('animated fadeOutRight');
-          } else if(theater.scrollTop() == 0) {
-            escHelp.removeClass('fadeOutRight').addClass('fadeInRight');
+              slideUrls.push(lastUrl);
+              thumbUrls.push(firstUrl);
+              // console.log("Largest URL:", lastUrl);
+            } else {
+              console.log("No url found.");
+            }
           }
-        });
 
-        $('#thumbs-list-handle').click(function() {
-          thumbsListContainer.toggleClass('closed');
-        })
+          console.log("Thumb URLs:", thumbUrls);
 
-        $('a.slide-thumb', '#theater-slides-list').click(function(e) {
-          e.preventDefault();
-          var theater = $('#ss-theater');
+          var slidesList = $("#theater-slides");
+          var thumbsListContainer = $("#theater-slides-list");
+          var thumbsList = $(".slide-thumbs", "#theater-slides-list");
+          totalSlides = slideUrls.length;
 
-          var scrollId = $(this).attr('href');
-          var height = $(scrollId).height();
-          var slideNumber = parseInt(scrollId.match(/#([0-9]+)/)[1]);
+          for (var i = 0; i <= slideUrls.length - 1; i++) {
+            slidesList.append(
+              '<li id="' +
+                (i + 1) +
+                '"><img src="' +
+                slideUrls[totalSlides - (i + 1)] +
+                '"/><span class="slide-number">' +
+                (i + 1) +
+                "/" +
+                slideUrls.length +
+                "</span></li>"
+            );
+            thumbsList.append(
+              '<li><a class="slide-thumb" href="#' +
+                (i + 1) +
+                '"><img src="' +
+                thumbUrls[totalSlides - (i + 1)] +
+                '"/></a></li>'
+            );
+          }
 
-          theater.animate({
-            scrollTop: (slideNumber - 1) * (height + 35)
-          }, 500);
-        });
+          setTimeout(function () {
+            theaterContent.show().addClass("animated fadeInUp");
+            thumbsListContainer.show().addClass("animated fadeInUp");
+          }, 600);
 
-      });
+          setTimeout(function () {
+            $("#esc-help").show().addClass("animated fadeInRight");
+          }, 1000);
+
+          setTimeout(function () {
+            thumbsListContainer.addClass("closed");
+          }, 3000);
+
+          theater.scroll(function () {
+            var escHelp = $("#esc-help");
+            if (theater.scrollTop() > 0) {
+              escHelp.addClass("animated fadeOutRight");
+            } else if (theater.scrollTop() == 0) {
+              escHelp.removeClass("fadeOutRight").addClass("fadeInRight");
+            }
+          });
+
+          $("#thumbs-list-handle").click(function () {
+            thumbsListContainer.toggleClass("closed");
+          });
+
+          $("a.slide-thumb", "#theater-slides-list").click(function (e) {
+            e.preventDefault();
+            var theater = $("#ss-theater");
+
+            var scrollId = $(this).attr("href");
+            var height = $(scrollId).height();
+            var slideNumber = parseInt(scrollId.match(/#([0-9]+)/)[1]);
+
+            theater.animate(
+              {
+                scrollTop: (slideNumber - 1) * (height + 35),
+              },
+              500
+            );
+          });
+        }
+      );
     } //Else block ends
 
     $(document).keyup(function (e) {
-      if (e.keyCode == 27) { closeTheater(); }
-    })
-
+      if (e.keyCode == 27) {
+        closeTheater();
+      }
+    });
 
     function closeTheater() {
-      var theater = $('#ss-theater');
-      var thumbsListContainer = $('#theater-slides-list');
+      var theater = $("#ss-theater");
+      var thumbsListContainer = $("#theater-slides-list");
 
-      theater.addClass('animated fadeOutDownBig');
-      thumbsListContainer.addClass('animated fadeOutDownBig');
-      setTimeout(function() {
+      theater.addClass("animated fadeOutDownBig");
+      thumbsListContainer.addClass("animated fadeOutDownBig");
+      setTimeout(function () {
         theater.remove();
         thumbsListContainer.remove();
-        $('body').removeClass('theater-running');
+        $("body").removeClass("theater-running");
       }, 600);
     }
   }
--- eventPage.js
+++ eventPage.js
@@ -1,13 +1,27 @@
-chrome.browserAction.onClicked.addListener( function (tab) {
-  startTheater();
-})
-chrome.commands.onCommand.addListener(function(command) {
-  if (command = 'toggle-ss-theater'){
-    startTheater();
+// chrome.runtime.onInstalled.addListener(() => {
+//   console.log("Extension installed and service worker is running!");
+// console.log(chrome.action);
+// });
+
+// Listen for browser action (now called action) clicks
+chrome.action.onClicked.addListener((tab) => {
+  startTheater(tab);
+});
+
+// Listen for keyboard shortcut commands
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "toggle-ss-theater") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length > 0) {
+        startTheater(tabs[0]);
+      }
+    });
   }
 });
-function startTheater(){
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {startTheater: true});
-  });
+
+// Function to start the theater mode
+function startTheater(tab) {
+  if (tab && tab.id) {
+    chrome.tabs.sendMessage(tab.id, { startTheater: true });
+  }
 }
